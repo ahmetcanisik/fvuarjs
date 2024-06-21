afb1c7d40516d2e8506608a71e144d1d10ca198e
ahmetcanisik <can.isik.business@gmail.com>
2024-06-21 21:34:03 +0300

    v1.4

--- public/src/components/footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="outbox">
-            <div className="panel">
-                &copy; 2024 ahmetcanisik
-            </div>
-        </footer>
-    );
-}
