05e4b0535e4eb18feb4e2df81c432f5ecbd7a6dd
ahmetcanisik <can.isik.business@gmail.com>
2024-06-23 18:55:57 +0300

    building test v1.4.3

---
+++ test/data.js
@@ -0,0 +1,314 @@
+if (!localStorage.getItem('preferences')) {
+    localStorage.setItem('preferences', JSON.stringify({ lang: 'en-US', theme: 'system', colors: "static" }));
+}
+const types = ["default", "error", "success", "info", "warning", "orange"];
+const posX = ["top-left", "top-center", "top-right", "center-left", "center", "center-right", "bottom-left", "bottom-center", "bottom-right"];
+const themeButtons = [
+    {
+        type: "default",
+        text: "Your Alert is here!",
+        time: 4,
+        position: "top-left"
+    },
+    {
+        type: "error",
+        text: "Your Alert is here!",
+        time: 4,
+        position: "bottom-left"
+    },
+    {
+        type: "success",
+        text: "Your Alert is here!",
+        time: 4,
+        position: "top-center"
+    },
+    {
+        type: "info",
+        text: "Your Alert is here!",
+        time: 3,
+        position: "bottom-center"
+    },
+    {
+        type: "warning",
+        text: "Your Alert is here!",
+        time: 1,
+        position: "top-right"
+    },
+    {
+        type: "orange",
+        text: "Your Alert is here!",
+        time: 2,
+        position: "bottom-right"
+    }
+]
+const content = {
+    icon: {
+        preferences: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75a4.5 4.5 0 0 1-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 1 1-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 0 1 6.336-4.486l-3.276 3.276a3.004 3.004 0 0 0 2.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852Z" /><path stroke-linecap="round" stroke-linejoin="round" d="M4.867 19.125h.008v.008h-.008v-.008Z" /></svg>`,
+        theme: {
+            title: `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M9.53 16.122a3 3 0 0 0-5.78 1.128 2.25 2.25 0 0 1-2.4 2.245 4.5 4.5 0 0 0 8.4-2.245c0-.399-.078-.78-.22-1.128Zm0 0a15.998 15.998 0 0 0 3.388-1.62m-5.043-.025a15.994 15.994 0 0 1 1.622-3.395m3.42 3.42a15.995 15.995 0 0 0 4.764-4.648l3.876-5.814a1.151 1.151 0 0 0-1.597-1.597L14.146 6.32a15.996 15.996 0 0 0-4.649 4.763m3.42 3.42a6.776 6.776 0 0 0-3.42-3.42" /></svg>`,
+        },
+    },
+    langs: [
+        {
+            short_name: "en-US",
+            long_name: "English",
+            icon: "🇺🇸"
+        },
+        {
+            short_name: "tr-TR",
+            long_name: "Turkish",
+            icon: "🇹🇷"
+        },
+        {
+            short_name: "az-AZ",
+            long_name: "Azerbaijani",
+            icon: "🇦🇿"
+        },
+        {
+            short_name: "ru-RU",
+            long_name: "Russian",
+            icon: "🇷🇺"
+        }
+    ],
+    "en-US": {
+        header: {
+            title: "Testing... Version 1.4",
+            preferences: "Preferences",
+            select_language: {
+                title: "Select Language",
+                close: "Close",
+                slogan: "modern alert library"
+            },
+            select_theme: {
+                title: "Select Theme",
+                themes: [
+                    {
+                        name: "system",
+                        value: "System",
+                        icon: "⚙️"
+                    },
+                    {
+                        name: "light",
+                        value: "Light",
+                        icon: "☀️"
+                    },
+                    {
+                        name: "dark",
+                        value: "Dark",
+                        icon: "🌙"
+                    }
+                ]
+            }
+        },
+        title: "Testing... v1.4",
+        copy_panel: {
+            title: 'Testing copyToClipboard function',
+            aria: 'Testing copy to clipboard function',
+            inp_placeholder: 'Enter the text you want to copy...',
+            btn_copy: 'copy to clipboard'
+        },
+        new_alert: {
+            title: 'Testing newAlert function',
+            aria: 'Testing new alert function',
+            theme_title: "Themes",
+            position_title: "Positions"
+        },
+        update_notes: {
+            title: "Update Notes",
+            version: {
+                v1_4: {
+                    title: "Version 1.4 (testing...)",
+                    list: [
+                        "Target attribute was added to the copyToClipboard function, it contains both the id and the element itself. The opportunity to copy content is provided by specifying.",
+                        "Fixed minor errors that occurred during copying.",
+                        "An animation (shake) has been added to the existence of alerts.",
+                        "Alerts colors and sizes have been rearranged to give them a more modernized look.",
+                        "It is now possible to change the maximum time that the alert will remain on the screen (maxTime) and the maximum number of alerts that will appear on the screen (maxCount)."
+                    ]
+                }
+            }
+            
+        }
+    },
+    "tr-TR": {
+        header: {
+            title: "Test edilen versiyon: 1.4",
+            preferences: "Tercihler",
+            select_language: {
+                title: "Dil Tercihi",
+                close: "Kapat",
+                slogan: "modern uyarı mesajları"
+            },
+            select_theme: {
+                title: "Tema Tercihi",
+                themes: [
+                    {
+                        name: "system",
+                        value: "Sistem",
+                        icon: "⚙️"
+                    },
+                    {
+                        name: "light",
+                        value: "Açık",
+                        icon: "☀️"
+                    },
+                    {
+                        name: "dark",
+                        value: "Koyu",
+                        icon: "🌙"
+                    }
+                ]
+            }
+        },
+        title: "Test edilen sürüm 1.4",
+        copy_panel: {
+            title: 'copyToClipboard fonksiyon testi',
+            aria: 'Panoya kopyala fonksiyonu testi',
+            inp_placeholder: 'Kopyalamak istediğiniz metni girin...',
+            btn_copy: 'kopyala'
+        },
+        new_alert: {
+            title: 'newAlert fonksiyon testi',
+            aria: 'yeni uyarı mesajı fonksiyonu testi',
+            theme_title: "Temalar",
+            position_title: "Pozisyonlar"
+        },
+        update_notes: {
+            title: "Güncelleme Notları",
+            version: {
+                v1_4: {
+                    title: "Versiyon 1.4 (test ediliyor...)",
+                    list: [
+                        "CopyToClipboard işlevine Target özelliği eklendi; hem kimliği hem de öğenin kendisini içerir. İçeriğin kopyalanma imkanı belirtilerek sağlanır.",
+                        "Kopyalama sırasında oluşan küçük hatalar düzeltildi.",
+                        "Uyarıların varlığına animasyon (sallama) eklendi.",
+                        "Uyarıların renkleri ve boyutları, onlara daha modern bir görünüm kazandırmak için yeniden düzenlendi.",
+                        "Artık uyarının ekranda kalacağı maksimum süreyi (maxTime) ve ekranda görünecek maksimum uyarı sayısını (maxCount) değiştirmek mümkün."
+                    ]
+                }
+            }
+            
+        }
+    },
+    "az-AZ": {
+        header: {
+            title: "Test edilmiş versiya: 1.4",
+            preferences: "Üstünlüklər",
+            select_language: {
+                title: "Dil üstünlükləri",
+                close: "Dörddəbir",
+                slogan: "müasir xəbərdarlıq mesajları"
+            },
+            select_theme: {
+                title: "Mövzuya üstünlük",
+                themes: [
+                    {
+                        name: "system",
+                        value: "Sistem",
+                        icon: "⚙️"
+                    },
+                    {
+                        name: "light",
+                        value: "Açıq",
+                        icon: "☀️"
+                    },
+                    {
+                        name: "dark",
+                        value: "Qaranlıq",
+                        icon: "🌙"
+                    }
+                ]
+            }
+        },
+        title: "Test edilmiş versiya 1.4",
+        copy_panel: {
+            title: 'copyToClipboard funksiya testi',
+            aria: 'Panoya kopyalayın funksiya testi',
+            inp_placeholder: 'Kopyalamaq istədiyiniz mətni daxil edin...',
+            btn_copy: 'surəti'
+        },
+        new_alert: {
+            title: 'newAlert funksiya testi',
+            aria: 'yeni xəbərdarlıq mesajı funksiya testi',
+            theme_title: "Mövzular",
+            position_title: "Vəzifələr"
+        },
+        update_notes: {
+            title: "Qeydləri yeniləyin",
+            version: {
+                v1_4: {
+                    title: "Versiya 1.4 (sınaq edilir...)",
+                    list: [
+                        "CopyToClipboard funksiyasına Hədəf xassəsi əlavə edildi; O, həm ID, həm də elementin özünü ehtiva edir. Məzmunu dəqiqləşdirməklə onun surətini çıxarmaq imkanı təmin edilir.",
+                        "Kopyalama zamanı baş verən kiçik səhvlər düzəldildi.",
+                        "Xəbərdarlıqların mövcudluğuna animasiya (silkələmə) əlavə edildi.",
+                        "Xəbərdarlıqların rəngləri və ölçüləri onlara daha müasir görünüş vermək üçün yenidən təşkil edilib.",
+                        "Artıq xəbərdarlığın ekranda qalacağı maksimum vaxtı (maxTime) və ekranda görünəcək xəbərdarlıqların maksimum sayını (maxCount) dəyişmək mümkündür."
+                    ]
+                }
+            }
+            
+        }
+    },
+    "ru-RU": {
+        header: {
+            title: "Тестирование... Версия 1.4",
+            preferences: "Предпочтения",
+            select_language: {
+                title: "Выберите язык",
+                close: "Закрывать",
+                slogan: "современная библиотека оповещений"
+            },
+            select_theme: {
+                title: "Выберите тему",
+                themes: [
+                    {
+                        name: "система",
+                        value: "Cистема",
+                        icon: "⚙️"
+                    },
+                    {
+                        name: "свет",
+                        value: "Cвет",
+                        icon: "☀️"
+                    },
+                    {
+                        name: "tемный",
+                        value: "Темный",
+                        icon: "🌙"
+                    }
+                ]
+            }
+        },
+        title: "Тестирование... v1.4",
+        copy_panel: {
+            title: 'Тестирование функции copyToClipboard',
+            aria: 'Тестирование функции копирования в буфер обмена',
+            inp_placeholder: 'Введите текст, который хотите скопировать...',
+            btn_copy: 'скопировать в буфер обмена'
+        },
+        new_alert: {
+            title: 'Тестирование функции newAlert',
+            aria: 'Тестирование новой функции оповещения',
+            theme_title: "Темы",
+            position_title: "Позиции"
+        },
+        update_notes: {
+            title: "Обновить примечания",
+            version: {
+                v1_4: {
+                    title: "Версия 1.4 (тестируется...)",
+                    list: [
+                        "В функцию copyToClipboard добавлен атрибут Target, он содержит как id, так и сам элемент. Возможность копирования контента предоставляется по указанию.",
+                        "Исправлены мелкие ошибки, возникавшие при копировании.",
+                        "К существованию оповещений добавлена ​​анимация (встряхивание).",
+                        "Цвета и размеры оповещений были изменены, чтобы придать им более современный вид.",
+                        "Теперь можно изменить максимальное время, в течение которого оповещение будет оставаться на экране (maxTime), и максимальное количество оповещений, отображаемых на экране (maxCount)."
+                    ]
+                }
+            }
+            
+        }
+    }
+}
+const preferences = JSON.parse(localStorage.getItem('preferences'));
---
+++ test/main.js
@@ -0,0 +1,306 @@
+$(document).ready(function () {
+
+    const create = new feJS({
+        maxCount: 4,
+        maxTime: 4
+    })
+    const cl = content[preferences.lang];
+    const stored = {
+        lang: preferences.lang || "en-US",
+        theme: preferences.theme || "system",
+        colors: preferences.colors || "static"
+    }
+    const box = $('.box');
+
+    if (stored.theme !== 'system') {
+        document.documentElement.dataset.theme = stored.theme;
+    } else {
+        document.documentElement.removeAttribute("data-theme");
+    }
+    document.documentElement.lang = stored.lang.split('-')[0];
+    $('title').text(cl.header.title);
+
+    function updatePreferences({ lang, theme, colors }) {
+        let pref = {
+            lang: lang || stored.lang,
+            theme: theme || stored.theme,
+            colors: colors || stored.colors
+        };
+        localStorage.setItem('preferences', JSON.stringify(pref));
+
+        if (pref.theme !== 'system') {
+            document.documentElement.dataset.theme = pref.theme;
+        } else {
+            document.documentElement.removeAttribute("data-theme");
+        }
+        location.reload();
+    }
+    
+    function copyPanel(parent) {
+        const copyPanel = $('<div class="panel copyPanel"></div>');
+        parent.append(copyPanel);
+
+        const copyTitle = $(`<h3 aria-label="${cl.copy_panel.aria}">${cl.copy_panel.title}</h3>`);
+        copyPanel.append(copyTitle);
+
+        const control = $('<div class="control"></div>');
+        copyPanel.append(control);
+
+        const testInp = $(`<input type="text" placeholder="${cl.copy_panel.inp_placeholder}" aria-label="${cl.copy_panel.inp_placeholder}"  class="inp" id="testInp">`);
+        control.append(testInp);
+
+        const btnCopy = $(`<button type="button" class="btn" id="btnCopy">${cl.copy_panel.btn_copy}</button>`);
+        control.append(btnCopy);
+
+        // copy to clipboard panel
+        btnCopy.click(() => {
+            create.copiedClipboard({
+                target: '#testInp',
+                type: "success",
+                time: 4,
+                position: "top-right"
+            })
+        })
+    }
+
+    function btnPanel(parent) {
+        const btnPanel = $('<fieldset class="panel btnPanel"></fieldset>');
+        parent.append(btnPanel);
+
+        const title = $(`<legend>${cl.new_alert.theme_title}</legend>`);
+        btnPanel.append(title);
+
+        // theme buttons
+        const ul = $('<ul></ul>');
+        for (const button of themeButtons) {
+            const li = $('<li></li>');
+            const btn = $(`<button type="button" class="btn ${button.type}">${button.type}</button>`);
+            btn.click(() => {
+                create.newAlert({
+                    type: button.type,
+                    text: button.text,
+                    time: button.time,
+                    position: button.position
+                })
+            })
+            li.append(btn);
+            ul.append(li);
+        }
+        btnPanel.append(ul);
+    }
+
+    function posPanel(parent) {
+        const posPanel = $(`<fieldset class="panel posPanel"></fieldset>`);
+        parent.append(posPanel);
+
+        const title = $(`<legend>${cl.new_alert.position_title}</legend>`);
+        posPanel.append(title);
+
+        // position buttons
+        const posList = $('<ul></ul>');
+        for (const pos of posX) {
+            const li = $('<li></li>');
+            const text = pos.replace('-', ' ');
+            let t = stored.colors === "static" ? "success" : types[Math.floor(Math.random() * types.length)];
+            const btn = $(`<button type="button" class="btn ${t}">${text}</button>`);
+            btn.click(() => {
+                create.newAlert({
+                    type: t,
+                    text: text,
+                    position: pos,
+                    time: 4
+                });
+            });
+            li.append(btn);
+            posList.append(li);
+        }
+        posPanel.append(posList);
+    }
+
+    function versions(parent) {
+        const version_info = $(`<legend><h3>${cl.update_notes.version.v1_4.title}</h3></legend>`);
+        parent.append(version_info);
+
+
+        const logs = $(`<ol></ol>`);
+        parent.append(logs);
+
+        cl.update_notes.version.v1_4.list.forEach(log => {
+            const li = $(`<li>${log}</li>`);
+            logs.append(li);
+        });
+    }
+
+    function langPanel(parent) {
+        const langTitle = $('<h3 class="text-left">' + `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" /></svg>` + cl.header.select_language.title + '</h3>');
+        parent.append(langTitle);
+        // Dil seçimini ekle
+        const language = $('<select name="language" class="inp" id="language"></select>');
+        parent.append(language);
+
+
+        language.change(() => {
+            updatePreferences({
+                lang: language.val()
+            })
+        })
+
+        const initialLang = content.langs.find(lang => lang.short_name === stored.lang);
+
+        if (initialLang) {
+            const first_opt = $(`<option value=${initialLang.short_name}><span class="icon">${initialLang.icon}<span> ${initialLang.long_name}</option>`);
+            language.append(first_opt);
+        }
+
+        content.langs.forEach(lang => {
+            if (lang.short_name !== stored.lang) {
+                const option = $(`<option value=${lang.short_name}><span class="icon">${lang.icon}<span> ${lang.long_name}</option>`);
+                language.append(option);
+            }
+        });
+    }
+
+    function themePanel(parent) {
+        const themeTitle = $('<h3 class="text-left">' + content.icon.theme.title + cl.header.select_theme.title + '</h3>');
+        parent.append(themeTitle);
+
+        const theme = $('<select name="theme" class="inp" id="theme"></select>');
+        parent.append(theme);
+
+        theme.change(() => {
+            updatePreferences({ theme: theme.val() })
+        })
+        
+        const initialTheme = cl.header.select_theme.themes.find(theme => theme.name === stored.theme);
+
+        if (initialTheme) {
+            const first_opt = $(`<option value=${initialTheme.name}><span class="icon">${initialTheme.icon}<span> ${initialTheme.value}</option>`);
+            theme.append(first_opt);
+        }
+
+        cl.header.select_theme.themes.forEach(them => {
+            if (them.name !== stored.theme) {
+                const option = $(`<option value=${them.name}><span class="icon">${them.icon}<span> ${them.value}</option>`);
+                theme.append(option);
+            }
+        });
+    }
+
+    function Dialog(parent) {
+        // Dil seçim butonunu oluştur ve parent elemana ekle
+        const $openLangDialog = $('<button class="openLangDialog btn">' + content.icon.preferences + cl.header.preferences + '</button>');
+        parent.append($openLangDialog);
+
+        // Dialog elemanını oluştur ve parent elemana ekle
+        const $dialog = $('<dialog></dialog>');
+        parent.append($dialog);
+
+
+        const title = $('<h2 class="text-center">' + content.icon.preferences + cl.header.preferences + '</h2>');
+        $dialog.append(title);
+
+        
+        langPanel($dialog);
+
+        themePanel($dialog);
+
+        // Kapatma butonunu oluştur ve dialoga ekle
+        const $closeLangDialog = $('<button type="button" id="closeLangDialog" class="btn close" autofocus>' + cl.header.select_language.close + '</button>');
+        $dialog.append($closeLangDialog);
+
+        // Dialog polyfill'i yükle
+        if (!('showModal' in HTMLDialogElement.prototype)) {
+            $.getScript('https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.5.4/dialog-polyfill.min.js', function () {
+                $('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dialog-polyfill/0.5.4/dialog-polyfill.min.css">').appendTo('head');
+                dialogPolyfill.registerDialog(parent[0]);
+            });
+        }
+
+        // Açma butonuna tıklama olayını ekle
+        $openLangDialog.click(() => {
+            $dialog[0].showModal();
+            $dialog.css("display", "flex");
+        });
+
+        // Kapatma butonuna tıklama olayını ekle
+        $closeLangDialog.click(() => {
+            $dialog[0].close();
+            $dialog.css("display", "none");
+        });
+    }
+
+    function logo(parent) {
+        const hgroup = $('<hgroup></hgroup>')
+        parent.append(hgroup);
+
+        const logo = $('<span class="logo">fvuarJS</span>');
+        hgroup.append(logo);
+
+        const span = $(`<span class="mini-logo">${cl.header.select_language.slogan}</span>`);
+        hgroup.append(span);
+    }
+
+    function header(parent) {
+        const header = $('<header class="header outbox"></header>');
+        parent.append(header);
+
+        const bar = $('<div class="bar"></div>');
+        header.append(bar);
+
+        logo(bar);
+
+        Dialog(bar);
+    }
+
+    function testArea(parent) {
+        const outbox = $('<fieldset class="outbox"></fieldset>');
+        parent.append(outbox);
+
+        const title = $(`<legend><h1>${cl.title}</h1></legend>`);
+        outbox.append(title);
+
+        copyPanel(outbox);
+
+        const newAlertPanelTitle = $(`<h3>${cl.new_alert.title}</h3>`);
+        outbox.append(newAlertPanelTitle);
+
+        btnPanel(outbox);
+
+        posPanel(outbox);
+    }
+
+    function updateNotes(parent) {
+        const updateNotes = $(`<fieldset class="outbox"></fieldset>`);
+        parent.append(updateNotes);
+
+        const title = $(`<legend><h2>${cl.update_notes.title}</h2></legend>`);
+        updateNotes.append(title);
+
+        const panel = $(`<fieldset class="panel"></fieldset>`);
+        updateNotes.append(panel);
+
+        versions(panel);
+    }
+
+    function footer(parent) {
+        const footer = $(`
+<footer class="outbox">
+    <div class="panel">
+        &copy; 2024 ahmetcanisik
+    </div>
+</footer>     
+`);
+        parent.append(footer);
+    }
+
+    function updateDOM(parent) {
+        header(parent);
+
+        testArea(parent);
+
+        updateNotes(parent);
+
+        footer(parent);
+    }
+    updateDOM(box);
+})
